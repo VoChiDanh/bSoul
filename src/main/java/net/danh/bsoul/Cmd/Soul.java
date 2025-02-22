d105f0edd3198180b9ee5b99bc2950858d1b4e99
Danh <vochidanh.hs.c@gmail.com>
2025-02-22 09:20:47 +0700

    bSoul 2.9.8 - If msg is null then don't send message (mob) - From now require java 17+ - Register addSoul to MythicMobs Mechanic (Drop support bSoulMMAddon) - Remove duplication event - Fix issue with mm skill - Add way to make give command silent - Add transfer **Add this to HELP.PLAYER** ```     - "&e/soul transfer <player> <amount> - Transfer soul to player | &3Permission: soul.transfer"     ``` How to use addSoul? ``` Skills: - addSoul{a=1} @target ~onDeath ```

--- src/main/java/net/danh/bsoul/Cmd/Soul.java
+++ src/main/java/net/danh/bsoul/Cmd/Soul.java
@@ -31,8 +31,24 @@ public class Soul extends CMDBase {
     @Override
     public void playerexecute(Player p, String[] args) {
         if (args.length == 0) {
-            sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("YOUR_SOUL"))
-                    .replace("%soul%", String.valueOf(Data.getSoul(p))));
+            sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("YOUR_SOUL")).replace("%soul%", String.valueOf(Data.getSoul(p))));
+        }
+
+        if (p.hasPermission("soul.transfer")) {
+            if (args.length == 3) {
+                if (args[0].equalsIgnoreCase("transfer")) {
+                    Player t = Bukkit.getPlayer(args[1]);
+                    if (t != null && p != t) {
+                        int amount = Number.getInt(args[2]);
+                        if (Data.getSoul(p) - amount >= 0) {
+                            Data.addSoul(t, amount);
+                            sendPlayerMessage(t, Objects.requireNonNull(getlanguagefile().getString("CHANGE_SOUL")).replace("%change%", "&a+").replace("%soul%", new DecimalFormat("###,###.###").format(amount)).replace("%player%", p.getName()));
+                            Data.removeSoul(p, amount);
+                            sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHANGE_SOUL")).replace("%change%", "&c-").replace("%soul%", new DecimalFormat("###,###.###").format(amount)).replace("%player%", t.getName()));
+                        }
+                    }
+                }
+            }
         }
         if (p.hasPermission("soul.user")) {
             if (args.length == 2) {
@@ -60,7 +76,7 @@ public class Soul extends CMDBase {
                     sendPlayerMessage(p, "&aReloaded");
                 }
             }
-            if (args.length == 4) {
+            if (args.length == 4 || args.length == 5) {
                 if (args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("remove")) {
                     Player target = Bukkit.getPlayer(args[2]);
                     if (target == null) {
@@ -71,33 +87,33 @@ public class Soul extends CMDBase {
                         Integer soul = Number.getInt(args[3]);
                         if (args[0].equalsIgnoreCase("add")) {
                             if (args[1].equalsIgnoreCase("soul")) {
-                                if (Data.addSoul(target, soul)) {
-                                    sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHANGE_SOUL")).replace("%change%", "&a+").replace("%soul%", String.format("%,d", soul)).replace("%player%", target.getName()));
+                                if (Data.addSoul(target, soul) && args.length == 4) {
+                                    sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHANGE_SOUL")).replace("%change%", "&a+").replace("%soul%", new DecimalFormat("###,###.###").format(soul)).replace("%player%", target.getName()));
                                 }
                             }
                             if (args[1].equalsIgnoreCase("max")) {
                                 Data.addSoulMax(target, soul);
-                                sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHANGE_MAX_SOUL")).replace("%change%", "&a+").replace("%soul%", String.format("%,d", soul)).replace("%player%", target.getName()));
+                                sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHANGE_MAX_SOUL")).replace("%change%", "&a+").replace("%soul%", new DecimalFormat("###,###.###").format(soul)).replace("%player%", target.getName()));
                             }
                         }
                         if (args[0].equalsIgnoreCase("remove")) {
                             if (args[1].equalsIgnoreCase("soul")) {
                                 Data.removeSoul(target, soul);
-                                sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHANGE_SOUL")).replace("%change%", "&c-").replace("%soul%", String.format("%,d", soul)).replace("%player%", target.getName()));
+                                sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHANGE_SOUL")).replace("%change%", "&c-").replace("%soul%", new DecimalFormat("###,###.###").format(soul)).replace("%player%", target.getName()));
                             }
                             if (args[1].equalsIgnoreCase("max")) {
                                 Data.removeSoulMax(target, soul);
-                                sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHANGE_MAX_SOUL")).replace("%change%", "&c-").replace("%soul%", String.format("%,d", soul)).replace("%player%", target.getName()));
+                                sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHANGE_MAX_SOUL")).replace("%change%", "&c-").replace("%soul%", new DecimalFormat("###,###.###").format(soul)).replace("%player%", target.getName()));
                             }
                         }
                         if (args[0].equalsIgnoreCase("set")) {
                             if (args[1].equalsIgnoreCase("soul")) {
                                 Data.setSoul(target, soul);
-                                sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHECK_SOUL")).replace("%player%", target.getName()).replace("%soul%", String.format("%,d", soul)));
+                                sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHECK_SOUL")).replace("%player%", target.getName()).replace("%soul%", new DecimalFormat("###,###.###").format(soul)));
                             }
                             if (args[1].equalsIgnoreCase("max")) {
                                 Data.setSoulMax(target, soul);
-                                sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHECK_MAX_SOUL")).replace("%player%", target.getName()).replace("%soul%", String.format("%,d", soul)));
+                                sendPlayerMessage(p, Objects.requireNonNull(getlanguagefile().getString("CHECK_MAX_SOUL")).replace("%player%", target.getName()).replace("%soul%", new DecimalFormat("###,###.###").format(soul)));
                             }
                         }
                     } else {
@@ -122,7 +138,7 @@ public class Soul extends CMDBase {
                     return;
                 }
                 int soul = Data.getSoul(target);
-                sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHECK_SOUL")).replace("%player%", target.getName()).replace("%soul%", String.format("%,d", soul)));
+                sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHECK_SOUL")).replace("%player%", target.getName()).replace("%soul%", new DecimalFormat("###,###.###").format(soul)));
             }
         }
         if (args.length == 1) {
@@ -143,32 +159,32 @@ public class Soul extends CMDBase {
                     if (args[0].equalsIgnoreCase("add")) {
                         if (args[1].equalsIgnoreCase("soul")) {
                             if (Data.addSoul(target, soul)) {
-                                sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHANGE_SOUL")).replace("%change%", "&a+").replace("%soul%", String.format("%,d", soul)).replace("%player%", target.getName()));
+                                sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHANGE_SOUL")).replace("%change%", "&a+").replace("%soul%", new DecimalFormat("###,###.###").format(soul)).replace("%player%", target.getName()));
                             }
                         }
                         if (args[1].equalsIgnoreCase("max")) {
                             Data.addSoulMax(target, soul);
-                            sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHANGE_MAX_SOUL")).replace("%change%", "&a+").replace("%soul%", String.format("%,d", soul)).replace("%player%", target.getName()));
+                            sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHANGE_MAX_SOUL")).replace("%change%", "&a+").replace("%soul%", new DecimalFormat("###,###.###").format(soul)).replace("%player%", target.getName()));
                         }
                     }
                     if (args[0].equalsIgnoreCase("remove")) {
                         if (args[1].equalsIgnoreCase("soul")) {
                             Data.removeSoul(target, soul);
-                            sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHANGE_SOUL")).replace("%change%", "&c-").replace("%soul%", String.format("%,d", soul)).replace("%player%", target.getName()));
+                            sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHANGE_SOUL")).replace("%change%", "&c-").replace("%soul%", new DecimalFormat("###,###.###").format(soul)).replace("%player%", target.getName()));
                         }
                         if (args[1].equalsIgnoreCase("max")) {
                             Data.removeSoulMax(target, soul);
-                            sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHANGE_MAX_SOUL")).replace("%change%", "&c-").replace("%soul%", String.format("%,d", soul)).replace("%player%", target.getName()));
+                            sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHANGE_MAX_SOUL")).replace("%change%", "&c-").replace("%soul%", new DecimalFormat("###,###.###").format(soul)).replace("%player%", target.getName()));
                         }
                     }
                     if (args[0].equalsIgnoreCase("set")) {
                         if (args[1].equalsIgnoreCase("soul")) {
                             Data.setSoul(target, soul);
-                            sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHECK_SOUL")).replace("%player%", target.getName()).replace("%soul%", String.format("%,d", soul)));
+                            sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHECK_SOUL")).replace("%player%", target.getName()).replace("%soul%", new DecimalFormat("###,###.###").format(soul)));
                         }
                         if (args[1].equalsIgnoreCase("max")) {
                             Data.setSoulMax(target, soul);
-                            sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHECK_MAX_SOUL")).replace("%player%", target.getName()).replace("%soul%", String.format("%,d", soul)));
+                            sendConsoleMessage(c, Objects.requireNonNull(getlanguagefile().getString("CHECK_MAX_SOUL")).replace("%player%", target.getName()).replace("%soul%", new DecimalFormat("###,###.###").format(soul)));
                         }
                     }
                 } else {
@@ -182,20 +198,61 @@ public class Soul extends CMDBase {
     public List<String> TabComplete(CommandSender sender, String[] args) {
         List<String> completions = new ArrayList<>();
         List<String> commands = new ArrayList<>();
-        if (sender.hasPermission("soul.admin")) {
-            if (args.length == 1) {
+        if (args.length == 1) {
+            if (sender.hasPermission("soul.admin")) {
                 commands.add("add");
                 commands.add("remove");
                 commands.add("set");
                 commands.add("reload");
                 commands.add("help");
-                StringUtil.copyPartialMatches(args[0], commands, completions);
             }
-        } else {
-            if (args.length == 1) {
-                commands.add("check");
-                StringUtil.copyPartialMatches(args[0], commands, completions);
+            if (sender.hasPermission("soul.user")) commands.add("check");
+            if (sender.hasPermission("soul.transfer")) commands.add("transfer");
+            StringUtil.copyPartialMatches(args[0], commands, completions);
+        } else if (args.length == 2) {
+            if (sender.hasPermission("soul.admin")) {
+                if (args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("remove")) {
+                    commands.add("soul");
+                    commands.add("max");
+                }
             }
+            if (sender.hasPermission("soul.user") || sender.hasPermission("soul.transfer"))
+                if (args[0].equalsIgnoreCase("check")
+                        || args[0].equalsIgnoreCase("transfer")) {
+                    for (Player p : Bukkit.getOnlinePlayers()) {
+                        commands.add(p.getName());
+                    }
+                }
+            StringUtil.copyPartialMatches(args[1], commands, completions);
+        } else if (args.length == 3) {
+            if (sender.hasPermission("soul.admin")) {
+                if (args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("remove")) {
+                    if (args[1].equalsIgnoreCase("soul") || args[1].equalsIgnoreCase("max")) {
+                        for (Player p : Bukkit.getOnlinePlayers()) {
+                            commands.add(p.getName());
+                        }
+                    }
+                }
+            }
+            if (sender.hasPermission("soul.transfer")) {
+                if (args[0].equalsIgnoreCase("transfer") && sender instanceof Player p) {
+                    for (int i = (Data.getSoul(p) - Data.getSoul(p) + 1); i <= Data.getSoul(p); i++) {
+                        commands.add(String.valueOf(i));
+                    }
+                }
+            }
+            StringUtil.copyPartialMatches(args[2], commands, completions);
+        } else if (args.length == 4) {
+            if (sender.hasPermission("soul.admin")) {
+                if (args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("remove")) {
+                    if (args[1].equalsIgnoreCase("soul") || args[1].equalsIgnoreCase("max")) {
+                        for (int i = 1; i <= 10; i++) {
+                            commands.add(String.valueOf(i));
+                        }
+                    }
+                }
+            }
+            StringUtil.copyPartialMatches(args[3], commands, completions);
         }
         Collections.sort(completions);
         return completions;
--- src/main/java/net/danh/bsoul/Mythic/addSoulSkill.java
+++ src/main/java/net/danh/bsoul/Mythic/addSoulSkill.java
@@ -7,21 +7,29 @@ import io.lumine.mythic.api.skills.SkillMetadata;
 import io.lumine.mythic.api.skills.SkillResult;
 import io.lumine.mythic.bukkit.BukkitAdapter;
 import net.danh.bsoul.Manager.Data;
+import net.danh.bsoul.bSoul;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class addSoulSkill implements ITargetedEntitySkill {
 
     protected final int amount;
 
-    public addSoulSkill(MythicLineConfig config){
-        amount = config.getInteger(new String[] {"amount", "a"},1);
+    public addSoulSkill(MythicLineConfig config) {
+        amount = config.getInteger(new String[]{"amount", "a"}, 1);
     }
+
     @Override
     public SkillResult castAtEntity(SkillMetadata skillMetadata, AbstractEntity target) {
         if (target.isPlayer()) {
             Player p = (Player) BukkitAdapter.adapt(target);
             if (p != null) {
-                Data.addSoul(p, amount);
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        Data.addSoul(p, amount);
+                    }
+                }.runTask(bSoul.getInstance());
             }
         }
         return SkillResult.SUCCESS;
